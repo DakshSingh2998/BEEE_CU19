c1530ec820bb30348f677d015e5b5fa06d20dc02
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2019-11-03 21:24:33 +0530

    Create Exp1.ino

---
+++ EXP1_LED_FLASHER/Exp1.ino
@@ -0,0 +1,13 @@
+void setup()
+{
+  pinMode(0, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
+© 2019 GitHub, Inc.
